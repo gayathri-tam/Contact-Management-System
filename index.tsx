79ec73d7eb3492c936140b967f80744d88e41a09
Gayathri <gayathri18025@gmail.com>
2026-09-24 12:41:49 +0530

    index.tsx

--- index.tsx
+++ index.tsx
@@ -19,10 +19,10 @@ type Contact = {
 };
 
 const API_URL =
-  'https://fastapi-contact-api-7bqo.onrender.com/contacts';
+  'https://contact-management-system-1-71qb.onrender.com/contacts';
 
 const AUTH_USERNAME = 'admin';
-const AUTH_PASSWORD = 'wrongpas';
+const AUTH_PASSWORD = 'wrongpassword';
 
 export default function HomeScreen() {
   // =========================
